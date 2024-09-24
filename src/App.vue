079ea698bc9188173cf03523e2e1d6f7e2eca883
Afong <t225310917@gmail.com>
2024-09-24 11:32:41 +0800

    env create

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/krtcMap">高雄捷運與輕軌地圖</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
