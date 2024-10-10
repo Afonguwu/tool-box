99e3129872ac6369469c01dd31f812e6291285c7
Afong <t225310917@gmail.com>
2024-10-10 15:27:02 +0800

    feat: 🎸 dark theme color style

--- src/components/StationPic.vue
+++ src/components/StationPic.vue
@@ -14,12 +14,14 @@ const {
 </script>
 <template>
   <svg :viewBox="viewBox" style="width: 100%; height: auto" xmlns="http://www.w3.org/2000/svg">
-    <text x="50" y="150" font-size="2rem" font-weight="bolder">高雄捷運輕軌路線圖</text>
+    <text x="50" y="150" font-size="2rem" font-weight="bolder" class="title-color">
+      高雄捷運輕軌路線圖
+    </text>
     {/* light rail */}
     <polyline
       :points="lightRailPoints"
       fill="none"
-      stroke="#7fbf3d"
+      class="green"
       stroke-width="10"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -28,7 +30,7 @@ const {
     <polyline
       :points="redLinePoints"
       fill="none"
-      stroke="#de0041"
+      class="red"
       stroke-width="10"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -37,7 +39,7 @@ const {
     <polyline
       :points="orangeLinePoints"
       fill="none"
-      stroke="#f49b0f"
+      class="orange"
       stroke-width="10"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -47,6 +49,7 @@ const {
         :x="station.x"
         :y="station.y"
         font-size="1rem"
+        class="text-color"
         :text-anchor="station.textAnchor"
         :transform="station.transform"
       >
@@ -57,7 +60,7 @@ const {
         :r="i === 28 ? 20 : r"
         :cx="station.cx"
         :cy="station.cy"
-        :fill="i === 28 ? 'none' : station.fee === 'boy' ? '#fefc4d' : '#fff'"
+        :fill="i === 28 ? 'none' : station.fee === 'boy' ? '#fefc4d' : '#ffffff'"
         :stroke-width="strokeWidth"
         class="site"
         :class="station.class"
@@ -85,44 +88,54 @@ const {
       x="1000"
       y="200"
       fill="none"
-      stroke="#c1c1c1"
+      class="border-color"
       rx="12"
       ry="12"
-      stroke-width="3"
+      stroke-width="2"
     />
-    <text x="1020" y="240" font-size="1.5rem">營運路線</text>
-    <line x1="1020" y1="250" x2="1300" y2="250" stroke="#c1c1c1" />
+    <text x="1020" y="240" font-size="1.5rem" class="text-color">營運路線</text>
+    <line x1="1020" y1="250" x2="1280" y2="250" class="border-color" />
     <circle r="18" cx="1040" cy="280" fill="#fff" stroke-width="5" class="red" />
-    <text x="1110" y="288">紅線（岡山車站 － 小港）</text>
+    <text x="1110" y="288" class="text-color">紅線（岡山車站 － 小港）</text>
     <circle r="18" cx="1040" cy="340" fill="#fff" stroke-width="5" class="orange" />
-    <text x="1110" y="348">橘線（哈瑪星 － 大寮）</text>
+    <text x="1110" y="348" class="text-color">橘線（哈瑪星 － 大寮）</text>
     <circle r="18" cx="1040" cy="400" fill="#fff" stroke-width="5" class="green" />
-    <text x="1110" y="408">輕軌</text>
+    <text x="1110" y="408" class="text-color">輕軌</text>
 
-    <text x="1020" y="460" font-size="1.5rem">圖示</text>
-    <line x1="1020" y1="470" x2="1300" y2="470" stroke="#c1c1c1" />
+    <text x="1020" y="460" font-size="1.5rem" class="text-color">圖示</text>
+    <line x1="1020" y1="470" x2="1280" y2="470" class="border-color" />
     <circle r="16" cx="1040" cy="500" fill="#fff" stroke-width="5" class="red" />
     <circle r="16" cx="1075" cy="500" fill="#fff" stroke-width="5" class="green" />
     <circle r="16" cx="1040" cy="550" fill="#fff" stroke-width="5" class="orange" />
     <circle r="16" cx="1075" cy="550" fill="#fff" stroke-width="5" class="green" />
-    <text x="1110" y="530">輕軌轉乘站</text>
+    <text x="1110" y="530" class="text-color">輕軌轉乘站</text>
     <circle r="16" cx="1044" cy="610" fill="#fff" stroke-width="5" class="red" />
     <circle r="20" cx="1044" cy="610" fill="none" stroke-width="5" class="orange" />
-    <text x="1110" y="618">交會站</text>
+    <text x="1110" y="618" class="text-color">紅橘線交會站</text>
   </svg>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
+.title-color {
+  stroke-width: 0.5;
+  stroke: var(--color-border);
+  fill: var(--color-mrt-orange);
+}
+.dot-fill {
+  fill: var(color-background-soft);
+}
+
 .site {
   cursor: pointer;
 }
+
 .red {
-  stroke: #de0041;
+  stroke: var(--color-mrt-red);
 }
 .orange {
-  stroke: #f49b0f;
+  stroke: var(--color-mrt-orange);
 }
 .green {
-  stroke: #7fbf3d;
+  stroke: var(--color-mrt-green);
 }
 .here {
   font-size: 1.5rem;
