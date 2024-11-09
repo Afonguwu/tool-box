cd55da87f412662d27171e56f14a9f601115f881
Afong <t225310917@gmail.com>
2024-11-09 13:33:11 +0800

    feat: 🎸 header edit

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,8 +1,7 @@
 <script setup></script>
 <template>
-  <header class="d-flex p-1 sticky-top">
-    <span class="material-symbols-outlined"> train </span>
-    <h1>高雄捷運輕軌路線圖</h1>
+  <header class="d-flex p-1">
+    <h1><span class="material-symbols-outlined">train</span>高雄捷運輕軌路線圖</h1>
   </header>
 </template>
 <style lang="scss" scoped>
@@ -10,14 +9,10 @@ header {
   color: var(--color-text);
   font-weight: bold;
   border-bottom: 1px solid var(--color-text);
-  display: flex;
-  align-items: center;
   h1 {
     margin: 0;
-  }
-  .material-symbols-outlined {
-    font-size: 2rem;
-    line-height: 1.2;
+    display: flex;
+    align-items: center;
   }
 }
 </style>
